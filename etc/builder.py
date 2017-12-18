6d9a68332a6687cf8337d503a207ebde79bf262a
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-12-18 13:14:06 +0000

    Add builder support

---
+++ etc/builder.py
@@ -0,0 +1,11 @@
+from iocbuilder import Device
+
+__all__ = ['ExcaliburDetector']
+
+
+class ExcaliburDetector(Device):
+
+    """Simply adds EXCALIBURDETECTOR path to configure/RELEASE."""
+
+    # Device attributes
+    AutoInstantiate = True
