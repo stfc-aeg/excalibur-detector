c58d352a7e232fcafed599ce91c1e4918ada6a09
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-12-18 17:20:15 +0000

    Update builder to store configuration information

--- etc/builder.py
+++ etc/builder.py
@@ -1,11 +1,22 @@
 from iocbuilder import Device
+from iocbuilder.arginfo import makeArgInfo, Simple
 
 __all__ = ['ExcaliburDetector']
 
 
 class ExcaliburDetector(Device):
 
-    """Simply adds EXCALIBURDETECTOR path to configure/RELEASE."""
+    """Store configuration for Excalibur Detector."""
+
+    NAME = "excalibur"
 
     # Device attributes
     AutoInstantiate = True
+
+    def __init__(self, MACRO):
+        self.__super.__init__()
+        # Update attributes with parameters
+        self.__dict__.update(locals())
+
+    ArgInfo = makeArgInfo(__init__,
+                          MACRO=Simple('Dependency MACRO as in configure/RELEASE', str))
