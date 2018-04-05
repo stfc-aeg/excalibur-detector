e5b2542dd428bb7e513a4958338b1bf182130f9b
Alan Greer <alan.greer@diamond.ac.uk>
2018-04-05 11:38:59 +0100

    Implemented builder class for receiver object.

--- etc/builder.py
+++ etc/builder.py
@@ -1,12 +1,30 @@
 from iocbuilder import Device
 from iocbuilder.arginfo import makeArgInfo, Simple
 
-__all__ = ['ExcaliburProcessPlugin']
+__all__ = ['ExcaliburProcessPlugin', 'ExcaliburReceiverPlugin']
 
 
 class ExcaliburProcessPlugin(Device):
 
     """Store configuration for ExcaliburProcessPlugin."""
+
+    NAME = "excalibur"
+
+    # Device attributes
+    AutoInstantiate = True
+
+    def __init__(self, MACRO):
+        self.__super.__init__()
+        # Update attributes with parameters
+        self.__dict__.update(locals())
+
+    ArgInfo = makeArgInfo(__init__,
+                          MACRO=Simple('Dependency MACRO as in configure/RELEASE', str))
+
+
+class ExcaliburReceiverPlugin(Device):
+
+    """Store configuration for ExcaliburReceiverPlugin."""
 
     NAME = "excalibur"
 
